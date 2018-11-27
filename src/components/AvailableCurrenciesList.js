1a631020bc1474c7625db61707721be7bfc8866d
dmitryreese <dmitry.reese@mail.ru>
2018-11-27 03:44:48 +0300

    Remove state from AvailableCurrenciesList

--- src/components/AvailableCurrenciesList.js
+++ src/components/AvailableCurrenciesList.js
@@ -6,14 +6,11 @@ import AvailableCurrencyItem from '../containers/AvailableCurrencyItem';
 class AvailableCurrenciesList extends Component {
     constructor(props) {
         super(props);
-        this.state = { isPopulated: !(this.props.items.length === 0) }
     }
 
     async componentDidMount() {
         if (this.props.items.length === 0) {
             const response = await fetch(baseUrl).then(data => data.json());
-
-            this.setState({ isPopulated: true })
             this.props.fetchAvailable(response);
         }
     }
@@ -22,7 +19,7 @@ class AvailableCurrenciesList extends Component {
         return (
             <div>
                 <div className={'list-header'}>Доступные валюты:</div>
-                {!this.state.isPopulated && <div>Загружаю...</div>}
+                {this.props.items.length === 0 && <div>Загружаю...</div>}
                 <div className={'list-items available-currencies'}>
                     {this.props.items.map((item, idx) => <AvailableCurrencyItem key={idx} item={item} />)}
                 </div>
